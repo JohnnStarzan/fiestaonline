6842d181a4ffc9a6082f8253f9e5212c10b52304
Valentino <vale92graveglia@live.it>
2015-03-07 09:24:49 +0100

    Update TransparentControl.cs

--- Launcher/Controlli Personalizzati/TransparentControl.cs
+++ Launcher/Controlli Personalizzati/TransparentControl.cs
@@ -1,8 +1,8 @@
-﻿namespace CustomControl
-{
-    using System;
-    using System.Windows.Forms;
+using System;
+using System.Windows.Forms;
 
+namespace Launcher
+{
     internal class TransparentControl : Control
     {
         public TransparentControl()
